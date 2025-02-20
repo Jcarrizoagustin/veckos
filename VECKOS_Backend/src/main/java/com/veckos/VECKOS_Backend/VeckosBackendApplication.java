168e783cea48d0e1bd497bb2d68aed9b4406d7cd
jcarrizoagustin <jcarrizoagustin@gmail.com>
2025-02-20 00:23:51 -0300

    first commit

---
+++ VECKOS_Backend/src/main/java/com/veckos/VECKOS_Backend/VeckosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veckos.VECKOS_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeckosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeckosBackendApplication.class, args);
+	}
+
+}
---
+++ VECKOS_Backend/src/test/java/com/veckos/VECKOS_Backend/VeckosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veckos.VECKOS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeckosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
